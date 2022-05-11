5fbd5677e46a42fa1919e14510d2dac51ee5c036
stevenovak123 <stevenovak0802@gmail.com>
2022-05-11 11:26:59 +0530

    Added a hero Section

---
+++ components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const Hero = () => {
+	return (
+		<div className='w-full h-screen bg-GreyColor'>
+			{/* //Todo:  Container */}
+			<div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+				<p>Hello, My name is </p>
+				<h1 className='text-4xl sm:text-7xl font-bold '>Steve Dsouza</h1>
+				<h2 className='text-4xl sm:text-7xl font-bold'>I'm a Web Developer.</h2>
+				<p className='py-4 max-w-[700px]'>
+					I'm a Fresher looking to Help develop websites just like. Currently
+					I'm foucsed on building responsive front-ends & learning Node.js
+					<div>
+						<button className='group border-2 border-black px-6 py-3 my-2 flex  items-center  hover:bg-black hover:text-white'>
+							View Work
+							<span className='group-hover:rotate-90 duration-300'>
+								<HiArrowNarrowRight className='ml-3 ' />
+							</span>
+						</button>
+					</div>
+				</p>
+			</div>
+		</div>
+	)
+}
+
+export default Hero
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<Navbar />
+			<Hero />
 		</div>
 	)
 }
