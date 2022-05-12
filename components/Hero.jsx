c73e7a485c7c705370217b2a574a474ff2b7bc6c
stevenovak123 <stevenovak0802@gmail.com>
2022-05-12 17:52:06 +0530

    Updated Nav and Hero Section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Link } from 'react-scroll/modules'
 
 const Hero = () => {
 	return (
-		<div className='w-full h-screen bg-GreyColor'>
-			{/* //Todo:  Container */}
+		<div name='Hero' className='w-full h-screen bg-GreyColor'>
 			<div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
 				<p>Hello, My name is </p>
 				<h1 className='text-4xl sm:text-7xl font-bold '>Steve Dsouza</h1>
@@ -18,7 +18,9 @@ const Hero = () => {
 					<button className='group border-2 border-black px-6 py-3 my-2 flex  items-center  hover:bg-black hover:text-white'>
 						View Work
 						<span className='group-hover:rotate-90 duration-300'>
-							<HiArrowNarrowRight className='ml-3' />
+							<Link to='Work' smooth={true} duration={500}>
+								<HiArrowNarrowRight className='ml-3' />
+							</Link>
 						</span>
 					</button>
 				</div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,23 +4,50 @@ import logo from '../assets/logo.png'
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { Link } from 'react-scroll/modules'
 const Navbar = () => {
 	const [toggleHamburger, setToggleHamburger] = useState(false)
 	const handleHamburgerClick = () => {
 		setToggleHamburger(!toggleHamburger)
 	}
 	return (
-		<div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-GreyColor text-black'>
+		<div className='z-50 fixed w-full h-[80px] flex justify-between items-center px-4 bg-GreyColor text-black'>
 			<div>
-				<Image src={logo} width={50} height={30} alt='logo' />
+				<Image
+					src={logo}
+					width={50}
+					height={30}
+					alt='logo'
+					layout='responsive'
+				/>
 			</div>
 			{/* //* Menu */}
 			<ul className='hidden md:flex'>
-				<li>Home</li>
-				<li>About</li>
-				<li>Skills</li>
-				<li>Work</li>
-				<li>Contact</li>
+				<li>
+					<Link to='Hero' smooth={true} duration={500}>
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link to='About' smooth={true} duration={500}>
+						About
+					</Link>
+				</li>
+				<li>
+					<Link to='Skills' smooth={true} duration={500}>
+						Skills
+					</Link>
+				</li>
+				<li>
+					<Link to='Work' smooth={true} duration={500}>
+						Work
+					</Link>
+				</li>
+				<li>
+					<Link to='Contact' smooth={true} duration={500}>
+						Contact
+					</Link>
+				</li>
 			</ul>
 			<div onClick={handleHamburgerClick} className='md:hidden z-10'>
 				{!toggleHamburger ? <FaBars /> : <FaTimes />}
@@ -29,14 +56,59 @@ const Navbar = () => {
 				className={
 					!toggleHamburger
 						? 'hidden'
-						: 'absolute top-0 left-0 w-full  h-screen bg-GreyColor flex flex-col justify-center  items-center'
+						: 'absolute z-50 top-0 left-0 w-full  h-screen bg-GreyColor flex flex-col justify-center  items-center'
 				}
 			>
-				<li className='py-6 text-4xl'>Home</li>
-				<li className='py-6 text-4xl'>About</li>
-				<li className='py-6 text-4xl'>Skills</li>
-				<li className='py-6 text-4xl'>Work</li>
-				<li className='py-6 text-4xl'>Contact</li>
+				<li className='py-6 text-4xl'>
+					<Link
+						onClick={handleHamburgerClick}
+						to='Hero'
+						smooth={true}
+						duration={500}
+					>
+						Home
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link
+						onClick={handleHamburgerClick}
+						to='About'
+						smooth={true}
+						duration={500}
+					>
+						About
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link
+						onClick={handleHamburgerClick}
+						to='Skills'
+						smooth={true}
+						duration={500}
+					>
+						Skills
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link
+						onClick={handleHamburgerClick}
+						to='Work'
+						smooth={true}
+						duration={500}
+					>
+						Work
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link
+						onClick={handleHamburgerClick}
+						to='Contact'
+						smooth={true}
+						duration={500}
+					>
+						Contact
+					</Link>
+				</li>
 			</ul>
 			<div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
 				<ul>
@@ -45,6 +117,7 @@ const Navbar = () => {
 							className='flex  justify-between items-center w-full text-black'
 							href='https://www.linkedin.com/in/stevenovak123/'
 							target='_blank'
+							rel='noreferrer'
 						>
 							LinkedIn <FaLinkedin size={30} />
 						</a>
@@ -54,6 +127,7 @@ const Navbar = () => {
 							className='flex  justify-between items-center w-full text-black'
 							href='https://github.com/stevenovak123'
 							target='_blank'
+							rel='noreferrer'
 						>
 							GitHub <FaGithub size={30} />
 						</a>
@@ -61,8 +135,9 @@ const Navbar = () => {
 					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px]  hover:ml-[-10px]  duration-100 hover:bg-green-600 '>
 						<a
 							className='flex  justify-between items-center w-full text-black'
-							href='https://www.linkedin.com/in/stevenovak123/'
+							href='mailto:stevenovak0802@gmail.com'
 							target='_blank'
+							rel='noreferrer'
 						>
 							Email <HiOutlineMail size={30} />
 						</a>
@@ -70,8 +145,9 @@ const Navbar = () => {
 					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px]  hover:ml-[-10px]  duration-100 hover:bg-slate-600 '>
 						<a
 							className='flex  justify-between items-center w-full text-black'
-							href='https://www.linkedin.com/in/stevenovak123/'
+							href='https://docs.google.com/document/d/1r3dmDnmuXoZ9pvA31lzmM0_FLnvx9RQ3bFUKj6dfEpI/edit?usp=sharing/'
 							target='_blank'
+							rel='noreferrer'
 						>
 							Resume <BsFillPersonLinesFill size={30} />
 						</a>
